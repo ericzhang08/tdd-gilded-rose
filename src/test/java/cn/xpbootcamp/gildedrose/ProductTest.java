5f7a91b9d057a6743470a4ca325213aa289beac7
ericzhang08 <yuzhangfish@163.com>
2020-05-14 07:58:08 +0800

    refactor:replace constructor function with factory

--- src/test/java/cn/xpbootcamp/gildedrose/ProductTest.java
+++ src/test/java/cn/xpbootcamp/gildedrose/ProductTest.java
@@ -13,7 +13,7 @@ public class ProductTest {
     public void should_quality_decrease_1_point_per_day_when_one_day_passed_given_on_date_normal_product() {
         int sellIn = 2;
         int quality = 5;
-        Product product = new Product("normal", sellIn, quality);
+        Product product = Product.create("normal", sellIn, quality);
         product.passDays(1);
         assertEquals(4, product.getQuality());
     }
@@ -22,7 +22,7 @@ public class ProductTest {
     public void should_quality_decrease_2_point_per_day_when_one_day_passed_given_an_out_of_date_normal_product() {
         int sellIn = 0;
         int quality = 5;
-        Product product = new Product("normal", sellIn, quality);
+        Product product = Product.create("normal", sellIn, quality);
         product.passDays(1);
         assertEquals(3, product.getQuality());
     }
@@ -31,7 +31,7 @@ public class ProductTest {
     public void should_quality_decrease_1_point_per_day_when_multiple_day_passed_given_on_date_normal_product() {
         int sellIn = 10;
         int quality = 10;
-        Product product = new Product("normal", sellIn, quality);
+        Product product = Product.create("normal", sellIn, quality);
         product.passDays(5);
         assertEquals(5, product.getQuality());
     }
@@ -40,7 +40,7 @@ public class ProductTest {
     public void should_quality_decrease_2_point_per_day_when_multiple_day_passed_given_an_out_of_date_normal_product() {
         int sellIn = 0;
         int quality = 10;
-        Product product = new Product("normal", sellIn, quality);
+        Product product = Product.create("normal", sellIn, quality);
         product.passDays(5);
         assertEquals(0, product.getQuality());
     }
@@ -49,7 +49,7 @@ public class ProductTest {
     public void should_quality_decrease_when_multiple_day_passed_given_an_on_date_product_become_an_out_of_date_normal_product() {
         int sellIn = 5;
         int quality = 20;
-        Product product = new Product("normal", sellIn, quality);
+        Product product = Product.create("normal", sellIn, quality);
         product.passDays(10);
         assertEquals(5, product.getQuality());
     }
@@ -58,7 +58,7 @@ public class ProductTest {
     public void should_quality_is_bigger_than_0_when_multiple_day_passed_given_and_product_quality_bigger_than_0() {
         int sellIn = 5;
         int quality = 5;
-        Product product = new Product("normal", sellIn, quality);
+        Product product = Product.create("normal", sellIn, quality);
         product.passDays(10);
         assertEquals(0, product.getQuality());
     }
@@ -68,7 +68,7 @@ public class ProductTest {
     public void should_quality_is_49_when_one_day_passed_given_an_product_quality_bigger_than_50() {
         int sellIn = 5;
         int quality = 100;
-        Product product = new Product("normal", sellIn, quality);
+        Product product = Product.create("normal", sellIn, quality);
         product.passDays(1);
         assertEquals(49, product.getQuality());
     }
@@ -77,7 +77,7 @@ public class ProductTest {
     public void should_quality_increase_one_point_per_day_when_multiple_day_passed_given_aged_brie_product_is_on_date() {
         int sellIn = 10;
         int quality = 10;
-        Product product = new Product("Aged Brie", sellIn, quality);
+        Product product = Product.create("Aged Brie", sellIn, quality);
         product.passDays(5);
         assertEquals(15, product.getQuality());
     }
@@ -87,7 +87,7 @@ public class ProductTest {
     public void should_quality_increase_two_point_per_day_when_multiple_day_passed_given_aged_brie_product_is_out_of_date() {
         int sellIn = 0;
         int quality = 10;
-        Product product = new Product("Aged Brie", sellIn, quality);
+        Product product = Product.create("Aged Brie", sellIn, quality);
         product.passDays(5);
         assertEquals(20, product.getQuality());
     }
@@ -96,7 +96,7 @@ public class ProductTest {
     public void should_biggest_quality_is_50__when_multiple_day_passed_given_aged_brie_product_quality_is_smaller_than_50_and_increase_to_50() {
         int sellIn = 0;
         int quality = 40;
-        Product product = new Product(AGED_BRIE, sellIn, quality);
+        Product product = Product.create(AGED_BRIE, sellIn, quality);
         product.passDays(10);
         assertEquals(50, product.getQuality());
     }
@@ -105,7 +105,7 @@ public class ProductTest {
     public void should_quality_increase_when_multiple_day_passed_given_an_on_date_product_become_an_out_of_date_aged_brie_product() {
         int sellIn = 5;
         int quality = 20;
-        Product product = new Product(AGED_BRIE, sellIn, quality);
+        Product product = Product.create(AGED_BRIE, sellIn, quality);
         product.passDays(10);
         assertEquals(35, product.getQuality());
     }
@@ -114,7 +114,7 @@ public class ProductTest {
     public void should_quality_does_not_change_when_multiple_days_passed_given_an_on_date_sulfuras_product() {
         int sellIn = 10;
         int quality = 20;
-        Product product = new Product(SULFURAS, sellIn, quality);
+        Product product = Product.create(SULFURAS, sellIn, quality);
         product.passDays(5);
         assertEquals(20, product.getQuality());
     }
@@ -123,7 +123,7 @@ public class ProductTest {
     public void should_quality_does_not_change_when_multiple_day_passed_given_an_on_date_product_become_an_out_of_date_sulfuras_product() {
         int sellIn = 5;
         int quality = 20;
-        Product product = new Product(SULFURAS, sellIn, quality);
+        Product product = Product.create(SULFURAS, sellIn, quality);
         product.passDays(10);
         assertEquals(20, product.getQuality());
     }
