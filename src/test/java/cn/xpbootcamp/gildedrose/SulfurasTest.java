13a6d79350a1bdec739783c6f4fc16f37246cf9a
ericzhang08 <yuzhangfish@163.com>
2020-05-18 11:57:19 +0800

    should_sellIn_n1_and_quality_49_when_one_day_pass_given_sulfuras_sellIn_0_quality_49

--- src/test/java/cn/xpbootcamp/gildedrose/SulfurasTest.java
+++ src/test/java/cn/xpbootcamp/gildedrose/SulfurasTest.java
@@ -22,4 +22,13 @@ public class SulfurasTest {
         assertEquals(sulfuras.getQuality(), 49);
     }
 
+    @Test
+    public void should_sellIn_n1_and_quality_49_when_one_day_pass_given_sulfuras_sellIn_0_quality_49() {
+        Sulfuras sulfuras = new Sulfuras(0, 49);
+        sulfuras.oneDayPass();
+        assertEquals(sulfuras.getSellIn(), -1);
+        assertEquals(sulfuras.getQuality(), 49);
+    }
+
+
 }
