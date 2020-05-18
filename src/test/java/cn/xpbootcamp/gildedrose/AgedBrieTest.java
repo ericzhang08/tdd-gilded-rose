87fbb09174c6ed58f2acd1ebaf01535befce20fa
ericzhang08 <yuzhangfish@163.com>
2020-05-18 11:47:06 +0800

    should_sellIn_0_and_quality_1_when_one_day_pass_given_Aged_Brie_sellIn_1_quality_0

--- src/test/java/cn/xpbootcamp/gildedrose/AgedBrieTest.java
+++ src/test/java/cn/xpbootcamp/gildedrose/AgedBrieTest.java
@@ -12,4 +12,12 @@ public class AgedBrieTest {
         assertEquals(agedBrie.getSellIn(), 4);
         assertEquals(agedBrie.getQuality(), 11);
     }
+
+    @Test
+    public void should_sellIn_0_and_quality_1_when_one_day_pass_given_Aged_Brie_sellIn_1_quality_0() {
+        AgedBrie agedBrie = new AgedBrie(1, 0);
+        agedBrie.oneDayPass();
+        assertEquals(agedBrie.getSellIn(), 0);
+        assertEquals(agedBrie.getQuality(), 1);
+    }
 }
